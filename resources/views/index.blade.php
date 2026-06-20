2f28bc6f9989817734504b03fb8ae236ff1ed443
Carlo-Falanga <carlofalanga7@gmail.com>
2026-06-20 09:20:27 +0200

    Update dependencies and configure Bootstrap with Vite; add Sass variables and styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,9 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tabellone Treni</title>
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-        rel="stylesheet">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
         rel="stylesheet">
